a363e547aefff71c63be5c1038ea3759a0a70f40
vadim <pldsrt@gmail.com>
2025-05-20 15:56:36 +0300

    task-10

--- task-10.fs
+++ task-10.fs
@@ -1,15 +1,15 @@
 type TimeOfDay = { hours: int; minutes: int; f: string }
-let (am, pm) = ("AM", "PM");
-
-let isLaterPeriod (x: string, y: string) =
-    x <> y && x = pm
 
 let isTimeValid (x: TimeOfDay) =
     let {hours = xHours; minutes = xMinutes; f = xF} = x
-    xHours >=0 && xHours <= 11 && xMinutes >= 0 && xMinutes <= 59 && (xF = am || xF = pm)
+    xHours >=0 && xHours <= 11 && xMinutes >= 0 && xMinutes <= 59 && (xF = "AM" || xF = "PM")
 
 let (.>.) (x:TimeOfDay) (y:TimeOfDay) =
     let {hours = xHours; minutes = xMinutes; f = xF} = x
     let {hours = yHours; minutes = yMinutes; f = yF} = y
-    if isTimeValid(x) && isTimeValid(y) then isLaterPeriod(xF, yF) || xHours > yHours || xMinutes > yMinutes
+    if isTimeValid(x) && isTimeValid(y) then
+        match xF, yF with
+            | "PM", "AM" -> true
+            | "AM", "PM" -> false
+            | _ -> xHours > yHours || xMinutes > yMinutes
     else failwith "Invalid time"
