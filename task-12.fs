5eea9c3c104577ef0f112ea843d008315cde5c55
vadim <pldsrt@gmail.com>
2025-05-21 13:50:51 +0300

    task-12

--- task-12.fs
+++ task-12.fs
@@ -12,15 +12,14 @@ let rec dnto n =
     else n :: dnto(n-1)
 
 // 34.3
-let rec evenn n =
-    if n <= 1 then []
-    elif n = 2 then [2]
-    elif n%2=0 then evenn (n-1) @ [n]
-    else evenn(n-1)
-
-// 34.3
 let evenn n =
     let rec even m =
         if m <= 0 then []
         else (2 * n - 2 * (m - 1)) :: even (m - 1)
     even n
+
+// let rec evenn n =
+//     if n <= 1 then []
+//     elif n = 2 then [2]
+//     elif n%2=0 then evenn (n-1) @ [n]
+//     else evenn(n-1)
