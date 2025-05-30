bdd33a603b4d44349335c5529cbe6dd5c1049957
pld000 <pld000@yandex.ru>
2025-05-30 18:40:44 +0300

    task-16

---
+++ task-16.fs
@@ -0,0 +1,13 @@
+// 42.3
+let rec allSubsets n k =
+    if k < 0 || k > n then
+        Set.empty
+    elif k = 0 then
+        Set.ofList [Set.empty]
+    elif n = 0 then
+        Set.empty
+    else
+        let subsetsWithoutN = allSubsets (n - 1) (k - 1)
+        let withN = subsetsWithoutN |> Set.map (fun subset -> Set.add n subset)
+        let withoutN = allSubsets (n - 1) k
+        Set.union withN withoutN
