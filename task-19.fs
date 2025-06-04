13ee6ca2f7ec9642df78ea6385bb0859631ecb4a
vadim <pldsrt@gmail.com>
2025-06-04 20:24:17 +0300

    task-19

--- task-19.fs
+++ task-19.fs
@@ -11,9 +11,9 @@ let rec fibo2 n c =
     if n <= 0 then
         c 0 1
     else
-        fibo2 (n - 1) (fun n1 -> fun n2 -> c n2 (n1 + n2))
+        fibo2 (n-1) (fun n1 n2 -> c n2 (n1 + n2))
 
 // 48.4.3
 let rec bigList n k =
-  if n=0 then k []
+  if n = 0 then k []
   else bigList (n-1) (fun res -> k(1::res))
