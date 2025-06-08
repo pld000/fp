9fe16dbbccdf49e132d2b8eca93a3d843354b404
vadim <pldsrt@gmail.com>
2025-06-08 10:28:05 +0300

    task-21

--- task-21.fs
+++ task-21.fs
@@ -1,17 +1,23 @@
-let factorial n =
-    let rec f x a =
-        if x <= 1 then a
-        else f (x - 1) (a * x)
-    f n 1
-
 // 50.2.1
-let fac_seq n = seq {
-    for i in 0 .. n do yield factorial i
-}
+let fac_seq =
+    seq {
+        let rec factorial = function
+            | 0 -> 1
+            | n -> n * factorial (n - 1)
+
+        let mutable i = 0
+        while true do
+            yield factorial i
+            i <- i + 1
+    }
 
 // 50.2.2
-let seq_seq n = seq {
-    for i in 0 .. n do
-        if i = 0 then yield 0
-        else yield! seq [-1 * i; i]
-}
+let seq_seq =
+    seq {
+        yield 0
+        let mutable n = 1
+        while true do
+            yield -n
+            yield n
+            n <- n + 1
+    }
